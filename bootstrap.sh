6efc64e4a963298f44599b277c0fea1353a27934
shuuji3 <shuuji3@gmail.com>
2016-11-17 22:16:07 +0900

    Change command orders and add echo messages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 # install brew
+echo installing homebrew...
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# install ansible
+echo installing ansible...
+brew install ansible
+
 # clone this mac-provisioning repository itself
+echo cloning mac-provisioning repository...
 mkdir ~/src/ && cd ~/src/
 git clone https://github.com/shuuji3/mac-provisioning.git
 cd mac-provisioning/
 
-# install ansible
-brew install ansible
-
 # play ansible
+echo play ansible🎶
 ansible-playbook -i hosts localhost.yaml -v
